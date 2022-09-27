30ab9d1da12357914d5be63388cbdc6fa02416f6
Brad <Brad.Chunn39@gmail.com>
2022-09-27 08:59:51 -0400

    Adding in the Coding

---
+++ Deliverable1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Deliverable1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool run = true;
+            while (run == true)
+            {
+                Console.Write("How many people are we making PB&J sandwiches for? :");
+                int numberofpoeples;
+                double slices, tablespoonsofpeantsbutter, jellyspoons;
+                numberofpoeples = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(+numberofpoeples + " sandwichs needed to feed " + numberofpoeples + " people's");
+                Console.WriteLine("Amount of ingredients needed To Make sandwichs............\n\n");
+                slices = numberofpoeples * 2;
+                Console.WriteLine("Slice of bread :" + slices);
+                tablespoonsofpeantsbutter = numberofpoeples * 2;
+                Console.WriteLine("Tablespoons of peanut butter :" + tablespoonsofpeantsbutter);
+                jellyspoons = numberofpoeples * 4;
+                Console.WriteLine("Teaspoons of jelly :" + jellyspoons);
+
+                Double laof = Convert.ToDouble(slices / 28);
+                double neededloaf = Math.Round(laof, 0, MidpointRounding.ToPositiveInfinity);
+                Console.WriteLine("\n\nNeeded loaf of bread is :" + (neededloaf));
+
+                Double jarsofpeanutbutter = Convert.ToDouble(tablespoonsofpeantsbutter / 32);
+                double neededjar = Math.Round(jarsofpeanutbutter, 0, MidpointRounding.ToPositiveInfinity);
+                Console.WriteLine("Needed Jar of peanut butter's is :" + (neededjar));
+
+                double jarsofjelly = Convert.ToDouble(jellyspoons / 48);
+                double neededjell = Math.Round(jarsofjelly, 0, MidpointRounding.ToPositiveInfinity);
+                Console.WriteLine("Needed Jar of jell's is :" + (neededjar));
+
+                Console.WriteLine("would like to start the program again Y/Yes ");
+                string answer = Console.ReadLine();
+                if (answer == "Y" || answer == "y" || answer == "yes" || answer == "Yes")
+                {
+                    run = true;
+                }
+                else
+                {
+                    run = false;
+                    Console.WriteLine("Goodbye ");
+                }
+            }
+
+        }
+    }
+}
---
+++ Deliverable2.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Deliverable2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool run = true;
+            double partySize, Price = 0, buffetprice = 9.99;
+            int coffe = 0, softdrink = 0, water = 0, shake = 0;
+            Console.WriteLine("Welcome to buffet restaurant!");
+            Console.Write("Enter the Party Size :");
+            partySize = Convert.ToInt32(Console.ReadLine());
+            while (run == true)
+            {
+                Console.WriteLine("Do you want order any Drink y/n");
+                string ans = Console.ReadLine();
+                if (ans == "y" || ans == "Y")
+                {
+                    Console.WriteLine("1-Coffee     $2.00");
+                    Console.WriteLine("2-softdrink  $3.00");
+                    Console.WriteLine("3-Water      $1.50");
+                    Console.WriteLine("4-shake      $0.99");
+                    int option = Convert.ToInt32(Console.ReadLine());
+                    if (option == 1)
+                    {
+                        coffe = coffe + 1;
+                        Price = Price + 2;
+                    }
+                    else if (option == 2)
+                    {
+                        softdrink = softdrink + 1;
+                        Price = Price + 3;
+                    }
+                    else if (option == 3)
+                    {
+                        water = water + 1;
+                        Price = (int)(Price + 1.50);
+                    }
+                    else if (option == 4)
+                    {
+                        shake = shake + 1;
+                        Price = Price + 0.99;
+                    }
+                }
+                else
+                {
+                    run = false;
+                }
+            }
+            Console.WriteLine("Your Order detials..................\n");
+            Console.WriteLine("Items\t  Quantity\t Price\t Total");
+            Console.WriteLine("Buffet      \t" + partySize + "\t" + " $0.99  : $" + (partySize * buffetprice).ToString("0.00"));
+            Console.WriteLine("softdrink   \t" + softdrink + "\t" + " $3.00  : $" + (softdrink * 3.00).ToString("0.00"));
+            Console.WriteLine("coffee      \t" + coffe + "\t" + " $2.00  : $" + (coffe * 2.00).ToString("0.00"));
+            Console.WriteLine("Water       \t" + water + "\t" + " $3.00  : $" + (water * 1.50).ToString("0.00"));
+            Console.WriteLine("Shake       \t" + shake + "\t" + " $0.99  : $" + (shake * 0.99).ToString("0.00"));
+            Console.WriteLine("\nTotal...........................: $" + ((softdrink * 3.00) + (coffe * 2.00) + (water * 1.50) + (shake * 0.99) + (buffetprice * partySize)).ToString("0.00"));
+
+        }
+    }
+}
